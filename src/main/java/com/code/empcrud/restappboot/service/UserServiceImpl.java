43f725a1f0c76e2f46d981e918550f4317a121b3
Yves-HAKIZIMANA <hkzmnyves1@gmail.com>
2024-02-07 21:28:21 +0200

    UserService Implementation

---
+++ src/main/java/com/code/empcrud/restappboot/service/UserServiceImpl.java
@@ -0,0 +1,46 @@
+package com.code.empcrud.restappboot.service;
+
+import com.code.empcrud.restappboot.entity.User;
+import com.code.empcrud.restappboot.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User getUserById(Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        return optionalUser.get();
+    }
+
+
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User updateUser(User user) {
+        User existingUser = userRepository.findById(user.getId()).get();
+        existingUser.setFirstName(user.getFirstName());
+        existingUser.setLastName(user.getLastName());
+        existingUser.setEmail(user.getEmail());
+        return userRepository.save(existingUser);
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+         userRepository.deleteById(userId);
+    }
+}
